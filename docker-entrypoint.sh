69a812b5c39e2f2f2e8307ffbff709ac4dc85143
Nicola Beghin <nicolabeghin@gmail.com>
2020-03-09 12:52:47 +0100

    rework for selenium grid

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-sleep 5
-
-selenium-side-runner -s http://chromedriver:4444 --output-directory /root/out /sides/*.side
+selenium-side-runner --server http://selenium-hub:4444/wd/hub --output-directory /root/out /sides/*.side
---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker-compose -f docker-compose-runner.yml up  selenium-side-runner
+
